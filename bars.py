686ca3b2ad349e1f3414eafcedd99746f1bea705
Dmitry <dimk00z@gmail.com>
2017-01-17 22:24:26 +0300

    вечерние правки

--- bars.py
+++ bars.py
@@ -1,68 +1,56 @@
 import json
 import requests
 import zipfile
-from os import replace, remove
+from os import replace, remove, path
 from math import radians, cos, sin, asin, sqrt
-
-
-class BarClass(object):
-
-    def __init__(self, name, adress, seats_count, longitude, latitude):
-        self.name = name
-        self.adress = adress
-        self.seats_count = seats_count
-        self.latitude = latitude
-        self.longitude = longitude
 
 
 def download_bars_zip_from_opmosru(url, zip_file_name):
     req = requests.get(url)
-    file_bars = open(zip_file_name, 'wb')
-    for chunk in req.iter_content(100000):
-        file_bars.write(chunk)
-    file_bars.close()
-    if file_bars:
-        return file_bars
+    with open(zip_file_name, 'wb') as zip_file:
+        for chunk in req.iter_content(100000):
+            zip_file.write(chunk)
+    if zip_file:
+        return zip_file
 
 
 def unzip_json(zip_file_name, json_file_name):
     if zipfile.is_zipfile(zip_file_name):
         zipfile.ZipFile(zip_file_name).extractall()
-        # в архиве один файл, распаковываем и переименовываем его
         replace(zipfile.ZipFile(zip_file_name).namelist()[0], json_file_name)
         remove(zip_file_name)
         return True
 
 
-def load_bars_data_from_json(json_file_name):
-    bars = []
-    try:
-        json_file = json.loads(open(json_file_name).read())
-        for j_object in json_file:
-            bar = BarClass(j_object['Name'], j_object['Address'],
-                           j_object['SeatsCount'],
-                           j_object['geoData']['coordinates'][0],
-                           j_object['geoData']['coordinates'][1])
-            bars.append(bar)
-        return bars
-    except ValueError:
+def load_json_from_file(filepath):
+    if not path.exists(filepath):
         return None
+    with open(filepath, 'r', encoding='cp1251') as file_handler:
+        return json.load(file_handler)
+
+
+def load_bars_data_from_json(json_file_name):
+    # try:
+    json_file = load_json_from_file(json_file_name)
+    bars = []
+    for j_object in json_file:
+
+        bars.append({'name': j_object['Name'],
+                     'address': j_object['Address'],
+                     'seats_count': j_object['SeatsCount'],
+                     'longitude': j_object['geoData']['coordinates'][0],
+                     'latitude': j_object['geoData']['coordinates'][1]
+                     })
+
+    return bars
 
 
 def get_biggest_bar(bars):
-    biggest_bar = bars[0]
-    for bar in bars:
-        if bar.seats_count > biggest_bar.seats_count:
-            biggest_bar = bar
-    return biggest_bar
+    return max(bars, key=lambda d: d['seats_count'])
 
 
 def get_smallest_bar(bars):
-    smallest_bar = bars[0]
-    for bar in bars:
-        if bar.seats_count < smallest_bar.seats_count:
-            smallest_bar = bar
-    return smallest_bar
+    return min(bars, key=lambda d: d['seats_count'])
 
 
 def distance_between_points(lon1, lat1, lon2, lat2):
@@ -77,15 +65,25 @@ def distance_between_points(lon1, lat1, lon2, lat2):
 
 def get_closest_bar(bars, longitude, latitude):
     distance = distance_between_points(longitude, latitude, bars[
-                                       0].longitude, bars[0].latitude)
+                                       0]["longitude"], bars[0]["latitude"])
     closest_bar = bars[0]
     for bar in bars[1:]:
         distance_to_bar = distance_between_points(
-            longitude, latitude, bar.longitude, bar.latitude)
+            longitude, latitude, bar["longitude"], bar["latitude"])
         if distance_to_bar < distance:
             distance = distance_to_bar
             closest_bar = bar
     return closest_bar
+
+
+def string_bar(bar):
+    return (("{} с {} количеством посадочных мест по адресу {} \n" +
+             "широта: {}, долгота {}").format(bar["name"],
+                                              bar["seats_count"],
+                                              bar["address"],
+                                              str(bar["longitude"]),
+                                              str(bar["latitude"])))
+
 
 if __name__ == '__main__':
     print("Добро пожаловать!")
@@ -96,18 +94,13 @@ if __name__ == '__main__':
     if bars_zip:
         unzip_json("bars.zip", "bars.json")
         bars_data = load_bars_data_from_json("bars.json")
+        print(bars_data)
         if bars_data:
             biggest_bar = get_biggest_bar(bars_data)
             smallest_bar = get_smallest_bar(bars_data)
-            print()
-            print("Самый большой бар : " + (biggest_bar.name) + " c " +
-                  str(biggest_bar.seats_count) +
-                  " количеством посадочных мест" +
-                  "по адресу: " + biggest_bar.adress)
-            print("Самый мАленький бар : " + (smallest_bar.name) + " c " +
-                  str(smallest_bar.seats_count) +
-                  " количеством посадочных мест" +
-                  "по адресу: " + smallest_bar.adress)
+            print(biggest_bar)
+            print("Самый большой бар : " + string_bar(biggest_bar))
+            print("Самый мАленький бар : " + string_bar(smallest_bar))
             answer = ""
             while answer != "n":
                 answer = input(
@@ -117,12 +110,9 @@ if __name__ == '__main__':
                     try:
                         longitude = float(input("Введите долготу: "))
                         latitude = float(input("Введите широту: "))
-                        closest_bar = get_closest_bar(
-                            bars_data, longitude, latitude)
-                        print("Ближайший бар :" + closest_bar.name +
-                              " по адресу: " + closest_bar.adress)
-                        print(str(closest_bar.longitude) +
-                              " " + str(closest_bar.latitude))
-                    except Exception:
+                        closest_bar = get_closest_bar(bars_data,
+                                                      longitude, latitude)
+                        print("Ближайший бар :" + string_bar(closest_bar))
+                    except ValueError:
                         print("Возможно ввод некорректен")
     print("Много не пейте=)")
