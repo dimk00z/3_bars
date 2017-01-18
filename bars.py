01e31cda8037995e8d0095fa67ecec4c94147ef9
Dmitry <dimk00z@gmail.com>
2017-01-18 09:47:47 +0300

    Удалил загрузку, добавил ввод пути к файлу

--- bars.py
+++ bars.py
@@ -4,33 +4,14 @@ import zipfile
 from os import replace, remove, path
 from math import radians, cos, sin, asin, sqrt
 
-
-def download_bars_zip_from_opmosru(url, zip_file_name):
-    req = requests.get(url)
-    with open(zip_file_name, 'wb') as zip_file:
-        for chunk in req.iter_content(100000):
-            zip_file.write(chunk)
-    if zip_file:
-        return zip_file
-
-
-def unzip_json(zip_file_name, json_file_name):
-    if zipfile.is_zipfile(zip_file_name):
-        zipfile.ZipFile(zip_file_name).extractall()
-        replace(zipfile.ZipFile(zip_file_name).namelist()[0], json_file_name)
-        remove(zip_file_name)
-        return True
-
-
-def load_json_from_file(filepath):
-    if not path.exists(filepath):
-        return None
-    with open(filepath, 'r', encoding='cp1251') as file_handler:
-        return json.load(file_handler)
+EARTH_RADIUS = 6371
 
 
 def load_bars_data_from_json(json_file_name):
-    json_file = load_json_from_file(json_file_name)
+    if not path.exists(json_file_name):
+        return None
+    with open(json_file_name, 'r', encoding='cp1251') as file_handler:
+        json_file = json.load(file_handler)
     bars = []
     for j_object in json_file:
         bars.append({'name': j_object['Name'],
@@ -43,21 +24,21 @@ def load_bars_data_from_json(json_file_name):
 
 
 def get_biggest_bar(bars):
-    return max(bars, key=lambda d: d['seats_count'])
+    return max(bars, key=lambda max_key: max_key['seats_count'])
 
 
 def get_smallest_bar(bars):
-    return min(bars, key=lambda d: d['seats_count'])
+    return min(bars, key=lambda min_key: min_key['seats_count'])
 
 
 def distance_between_points(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    sub_result = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    sub_result = sin(dlat / 2) ** 2 + cos(lat1) * \
+        cos(lat2) * sin(dlon / 2) ** 2
     sub_result = 2 * asin(sqrt(sub_result))
-    earth_radius = 6371
-    return sub_result * earth_radius
+    return sub_result * EARTH_RADIUS
 
 
 def get_closest_bar(bars, longitude, latitude):
@@ -86,30 +67,23 @@ def string_bar(bar):
 
 if __name__ == '__main__':
     print("Добро пожаловать!")
-    print("Сейчас попробую загрузить бары с http://op.mos.ru")
-    bars_zip = download_bars_zip_from_opmosru(
-        'http://op.mos.ru/EHDWSREST/catalog/export/get?id=84505',
-        "bars.zip")
-    if unzip_json("bars.zip", "bars.json"):
-        bars_data = load_bars_data_from_json("bars.json")
-        if bars_data:
-            biggest_bar = get_biggest_bar(bars_data)
-            smallest_bar = get_smallest_bar(bars_data)
-            print(biggest_bar)
-            print("Самый большой бар : " + string_bar(biggest_bar))
-            print("Самый мАленький бар : " + string_bar(smallest_bar))
-            answer = ""
-            while answer != "n":
-                answer = input(
-                    "Вы желаете узнать ближайший бар" +
-                    " к заданным координатам? (y/n) :").lower()
-                if answer == "y":
-                    try:
-                        longitude = float(input("Введите долготу: "))
-                        latitude = float(input("Введите широту: "))
-                        closest_bar = get_closest_bar(bars_data,
-                                                      longitude, latitude)
-                        print("Ближайший бар :" + string_bar(closest_bar))
-                    except ValueError:
-                        print("Ввод некорректен")
-    print("Много не пейте=)")
+    bars = load_bars_data_from_json(input("Введите путь к json-файлу\n"))
+    if bars:
+        print("Самый большой бар : " + string_bar(get_biggest_bar(bars)))
+        print("Самый маленький бар : " + string_bar(get_smallest_bar(bars)))
+        answer = ""
+        while answer != "n":
+            answer = input(
+                "Вы желаете узнать ближайший бар \
+к заданным координатам? (y/n) :").lower()
+            if answer == "y":
+                try:
+                    longitude = float(input("Введите долготу: "))
+                    latitude = float(input("Введите широту: "))
+                    closest_bar = get_closest_bar(bars,
+                                                  longitude, latitude)
+                    print("Ближайший бар :" + string_bar(closest_bar))
+                except ValueError:
+                    print("Ввод некорректен")
+        else:
+            print("Файл не найден")
